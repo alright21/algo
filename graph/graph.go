e57ed594ad2eefecff257085e560012fa78bc0af
alright21 <alberto.giust@studenti.unitn.it>
2021-05-31 11:57:52 +0300

    feat(graph): add graph struct

---
+++ graph/graph.go
@@ -0,0 +1,14 @@
+package graph
+
+// Graph is a struct representing a graph, with int value as nodes and adjancency lists for edges
+type Graph struct {
+
+	Visited map[int]bool
+	Adj map[int][]int 
+}
+
+// AddEdge adds an edge from source to destination
+func (g *Graph) AddEdge(source int, destination int){
+
+	g.Adj[source] = append(g.Adj[source], destination)
+}
